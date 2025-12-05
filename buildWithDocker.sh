e638936ac8030c59ff3f418b39bfb208fafd9503
patkag <14110551+patkag@users.noreply.github.com>
2025-12-05 13:15:25 +0200

    optimizing docker build command

--- buildWithDocker.sh
+++ buildWithDocker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker run -d -it --name pico-sdk --mount type=bind,source=${PWD}/PocketWX,target=/home/dev lukstep/raspberry-pi-pico-sdk:latest
-
 mkdir -p ./PocketWX/build
 
-docker exec -it pico-sdk /bin/sh -c "cd /home/dev/build && cmake .. && make -j4 && exit"
+docker run -d -it --rm --name pico-sdk --mount type=bind,source=${PWD}/PocketWX,target=/home/dev lukstep/raspberry-pi-pico-sdk:latest /bin/sh -c "cd /home/dev/build && cmake .. && make -j4 && exit"
